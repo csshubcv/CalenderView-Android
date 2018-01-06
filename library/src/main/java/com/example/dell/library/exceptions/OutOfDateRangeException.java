53b709efe34e18bf09b3e24ecfbe180c6ca464e6
csshubcv <35163072+csshubcv@users.noreply.github.com>
2018-01-06 17:20:16 +0530

    Update OutOfDateRangeException.java

--- library/src/main/java/com/example/dell/library/exceptions/OutOfDateRangeException.java
+++ library/src/main/java/com/example/dell/library/exceptions/OutOfDateRangeException.java
@@ -1,8 +1,5 @@
 package com.example.dell.library.exceptions;
 
-/**
- * Created by Mateusz Kornakiewicz on 27.10.2017.
- */
 
 public class OutOfDateRangeException extends Exception {
     public OutOfDateRangeException(String message) {
