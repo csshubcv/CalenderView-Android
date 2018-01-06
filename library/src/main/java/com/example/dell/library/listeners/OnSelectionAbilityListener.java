065390d53b6373fe4b6bfb93af94715143e70978
csshubcv <35163072+csshubcv@users.noreply.github.com>
2018-01-06 17:26:01 +0530

    Update OnSelectionAbilityListener.java

--- library/src/main/java/com/example/dell/library/listeners/OnSelectionAbilityListener.java
+++ library/src/main/java/com/example/dell/library/listeners/OnSelectionAbilityListener.java
@@ -2,8 +2,6 @@ package com.example.dell.library.listeners;
 
 /**
  * This interface is used to inform DatePicker that user selected any days
- *
- * Created by Mateusz Kornakiewicz on 24.10.2017.
  */
 
 public interface OnSelectionAbilityListener {
